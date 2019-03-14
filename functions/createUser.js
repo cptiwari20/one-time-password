ef848d028bc06867fdeec86e988520283780bb59
Chandra Prakash Tiwari <cptiwari20@gmail.com>
2019-03-14 12:35:02 +0530

    added create user function

--- functions/createUser.js
+++ functions/createUser.js
@@ -1,3 +1,17 @@
+const admin = require('firebase-admin');
+
 module.exports = function(req, res){
-  res.send(req.body)
+  // verify the user provided the phone number
+  if(!req.body.phone){
+    return res.status(422).send({ error: "Bad input"});
+  }
+  // format that phone number and remove the dashes and symbol
+  const phone = String(req.body.phone).replace(/[^\d]/g, "");
+  
+  // create user using that phone number
+  admin.auth().createUser({ uid: phone})
+    .then((user) => res.send(user))
+    .then(err => res.status(422).send({error: err}));
+
+
 }
