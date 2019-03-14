5785036511360749d947581b20f7d389bf9ff406
Chandra Prakash Tiwari <cptiwari20@gmail.com>
2019-03-14 10:22:02 +0530

    added amin intialization

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,11 @@
 const functions = require('firebase-functions');
 const createUser = require('./createUser');
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceConfig.json')
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://one-time-password-9cc57.firebaseio.com"
+});
 
 exports.createUser = functions.https.onRequest(createUser);
